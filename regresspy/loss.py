21248860fa0e6eed8388bbc9576d722e76ad7827
Azharul Islam <azharul35-2329@diu.edu.bd>
2021-08-17 19:41:36 +0600

    Here the all regression project one file

--- regresspy/loss.py
+++ regresspy/loss.py
@@ -1,32 +1,18 @@
 import numpy as np
 from numpy import ndarray
 
-""" First of all create two variable and initial the the array value of those variable """
-"""Then finally run the each function """
-
-
-def _error(actual: np.ndarray, predicted: np.ndarray):
-    """ Error function """
-    return actual - predicted
-
 
 def mae(pred: ndarray, label: ndarray) -> ndarray:
-
-    """ Mae function """
-    return np.mean(np.abs(_error(label, pred)))
+    return np.mean(np.abs(label - pred))
 
 
 def sse(pred: ndarray, label: ndarray) -> ndarray:
-    """ sse function """
-    return np.sum((label-pred)**2)
+    return np.sum(np.power((label - pred), 2))
 
 
 def mse(pred: ndarray, label: ndarray) -> ndarray:
-    """ mse function """
-    return np.mean(np.square(_error(label, pred)))
+    return np.mean(np.power((label - pred), 2))
 
 
 def rmse(pred: ndarray, label: ndarray) -> ndarray:
-
-    """ rmse error """
-    return np.sqrt(mse(label, pred))
+    return np.sqrt(np.mean(np.power((label - pred), 2)))
